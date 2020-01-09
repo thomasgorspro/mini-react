14933ddb9894ef6d8a39367b74b7fe6d68459066
Thomas Gorszczyk <Thomasgo_1@hotmail.fr>
2020-01-09 11:04:17 +0100

    should not be pushed

--- router.js
+++ router.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var Router = {
+    routes = [],
+    root: '/',
+
+    event() {
+        window.addEventListener('popstate', function(event) {
+            
+        })
+    }
+}
+
