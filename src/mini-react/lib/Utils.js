6ceb4b18d23f487bfee17a10b121c5825c8c6860
Tom Délié <tomdelie78@gmail.com>
2020-02-12 17:46:00 +0100

    Add propAccess to Object prototype

--- src/mini-react/lib/Utils.js
+++ src/mini-react/lib/Utils.js
@@ -1,3 +1,18 @@
+Object.prototype.propAccess = function (prop) {
+    if (typeof prop !== 'string') {
+        return `Property ${prop} is not a string`;
+    }
+    const properties = prop.split('.');
+
+    if (this.hasOwnProperty(properties[0]) && (this[properties[0]] instanceof Object) && properties.length > 1) {
+        const property = this[properties[0]];
+        properties.splice(0,1);
+        return property.propAccess(properties.join('.'));
+    } else {
+        return this[properties[0]];
+    }
+}
+
 function type_check_v1(input, type) {
     if (input === null && type === "null" || typeof input === undefined && type === "undefined") {
         return true
